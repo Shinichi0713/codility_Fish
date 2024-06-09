620169cc479f577311e05e68fde628b1dcc7b4a9
Shinichi Yoshinaga <61480734+Shinichi0713@users.noreply.github.com>
2024-06-09 15:59:47 +0900

    Add files via upload

--- solution.py
+++ solution.py
@@ -1,37 +1,14 @@
-# you can write to stdout for debugging purposes, e.g.
-# print("this is a debug message")
-
-# 魚の動きのシミュレーション
 def solution(A, B):
-    # Implement your solution here
-    C = [0 for i in range(len(B))]
-    for i in range(len(B)):
-        # meetするか確認
-        if B[i]==0:
-            C[i]=i-1
-            if C[i] < 0:
-                C[i] = 0
-        else:
-            C[i]=i+1
-    while True:
-        is_change = 0
-        for i in range(len(B) - 1):
-            if A[i] == 0 or A[i+1] == 0:
-                continue
-            if C[i] <= C[i+1]:
-                if A[i] > A[i+1]:
-                    A[i] = 0
-                    C[i] += 1
-                    is_change = 1
-                else:
-                    A[i+1] = 0
-                    C[i] += -1
-                    is_change = 1
-        if is_change == 0:
-            break
-    num_alive = 0
+    downstream = []
+    alive_count = 0
     for i in range(len(A)):
-        if A[i] > 0:
-            num_alive += 1
-    return num_alive
-        
+        if B[i] == 1:
+            downstream.append(A[i])
+        # ではない場合、stockの中身確認
+        else:
+            while downstream and downstream[-1] < A[i]:
+                downstream.pop()
+            if not downstream:
+                alive_count += 1
+    alive_count += len(downstream)
+    return alive_count
